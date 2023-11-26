c510a042b3be531bde32c54c8b7ad133fc7f810f
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2023-11-26 13:08:59 -0600

    ➕ Add gping in tmux

--- tmux-all-the-things.sh
+++ tmux-all-the-things.sh
@@ -6,6 +6,7 @@ set -eu
 tmux has-session || tmux new-session -d
 tmux list-windows -f '#{m:mtr,#W}' | grep -q . || tmux new-window -n mtr mmtr $REMOTE
 tmux list-windows -f '#{m:stream,#W}' | grep -q . || tmux new-window -n stream nnload
+tmux list-windows -f '#{m:gping,#W}' | grep -q . || tmux new-window -n gping gping $REMOTE
 tmux select-window -t stream
 tmux list-panes | wc -l | grep -q ^1$ && tmux split-window -v -b -l 10
 #pidof -sq ffmpeg || tmux new-window -n kiyo kiyo.sh
