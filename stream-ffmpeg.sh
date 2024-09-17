59526951507cfc4638dc00a33f75a7fa664ea094
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2024-09-17 13:33:57 +0200

    🔛 Add ability to switch between RTMP and RTSP

--- stream-ffmpeg.sh
+++ stream-ffmpeg.sh
@@ -66,12 +66,8 @@ echo "$CODECINFO"
 FPS=30
 
 # Note: to see alsa devices and their numbers, run "arecord -l"
-# Note: to see the device names used here, run "/sys/class/sound/card*/id"
-#AUDIO_INPUT_NAME=ATR2USB
-#AUDIO_INPUT_NAME=UDULTCDL
-#AUDIO_INPUT_NAME=C4K
-#AUDIO_INPUT_NAME=PCH
-AUDIO_INPUT_NAME=RX
+# Note: to see the device names used here, check "/sys/class/sound/card*/id"
+echo "AUDIO_INPUT_NAME=${AUDIO_INPUT_NAME:=undefined}"
 CARD_NUMBER=
 for CARD in /sys/class/sound/card*; do
   if grep -q "$AUDIO_INPUT_NAME" $CARD/id; then
@@ -91,7 +87,10 @@ for CARD in /sys/class/sound/card*; do
   fi
 done
 if [ "$CARD_NUMBER" = "" ]; then
-  echo "Could not find ALSA card $AUDIO_INPUT_NAME. Aborting."
+  echo "Could not find ALSA card '$AUDIO_INPUT_NAME'."
+  echo "Available ALSA cards:"
+  cat /sys/class/sound/card*/id
+  echo "Please set environment variable AUDIO_INPUT_NAME accordingly."
   exit 1
 fi
 AUDIO_INPUT_MUSIC="-re -i $HOME/Dropbox/obs-assets/music/Sonnentanz.m4a"
@@ -99,29 +98,30 @@ AUDIO_INPUT_PULSE="-f pulse -i alsa_input.usb-UC_MIC_ATR2USB-00.analog-stereo"
 AUDIO_INPUT_PULSE="-f pulse -i alsa_input.usb-DJI_Technology_Co.__Ltd._Wireless_Microphone_RX-00.analog-stereo"
 AUDIO_INPUT_ALSA_DEFAULT="-f alsa -i default"
 
-OBS_VIRTUAL_DEVICE_NAME="OBS"
-OBS_VIRTUAL_DEVICE=""
-while [ -z "$OBS_VIRTUAL_DEVICE" ]; do
-  for dev in /sys/class/video4linux/*; do
-    if [ "$(cat $dev/name)" = "$OBS_VIRTUAL_DEVICE_NAME" ]; then
-      OBS_VIRTUAL_DEVICE="/dev/$(basename $dev)"
-      echo "VIDEO: type=V4L2, name=$OBS_VIRTUAL_DEVICE_NAME, device=$OBS_VIRTUAL_DEVICE"
-      break
-    fi
-  done
-  if [ -z "$OBS_VIRTUAL_DEVICE" ]; then
-    echo "Waiting for OBS Virtual Camera device (it has to be named '$OBS_VIRTUAL_DEVICE_NAME')..."
-    sleep 1
+echo "VIDEO_INPUT_NAME=${VIDEO_INPUT_NAME:=undefined}"
+VIDEO_INPUT_DEVICE=""
+for dev in /sys/class/video4linux/*; do
+  if grep -q "$VIDEO_INPUT_NAME" $dev/name; then
+    VIDEO_INPUT_DEVICE="/dev/$(basename $dev)"
+    echo "VIDEO: type=V4L2, name=$VIDEO_INPUT_NAME, device=$VIDEO_INPUT_DEVICE"
+    break
   fi
 done
+if [ -z "$VIDEO_INPUT_DEVICE" ]; then
+  echo "Could not find V4L2 device '$VIDEO_INPUT_NAME'."
+  echo "Available V4L2 devices:"
+  cat /sys/class/video4linux/*/name
+  echo "Please set environment variable VIDEO_INPUT_NAME accordingly."
+  exit 1
+fi
 
-VIDEO_INPUT_OBS="-f v4l2 -frame_size 1920x1080 -framerate $FPS -i $OBS_VIRTUAL_DEVICE"
+VIDEO_INPUT_V4L2="-f v4l2 -frame_size 1920x1080 -framerate $FPS -i $VIDEO_INPUT_DEVICE"
 VIDEO_INPUT_TEST="-re -f lavfi -i testsrc=size=hd1080:rate=30:decimals=1"
 INPUT_LOOP="-re -fflags +genpts -stream_loop -1 -i loop.mkv"
 VIDEO_INPUT_CLOCK="-re -f lavfi -i color=color=white:s=hd1080:r=30[white];movie=CLOCK.JPG,scale=hd1080:force_original_aspect_ratio=decrease[img];[white][img]overlay,drawtext=text=%{localtime}:fontcolor=black:x=w-text_w-96:y=(h-text_h)/2:fontsize=48,drawtext=text=Playing→:fontcolor=black:x=w-text_w-32:y=h-3*text_h-32:fontsize=32,drawtext=textfile=title.txt:fontcolor=black:x=w-text_w-32:y=h-text_h-32:fontsize=32'"
 
 AUDIO_INPUT="$AUDIO_INPUT_ALSA"
-VIDEO_INPUT="$VIDEO_INPUT_OBS"
+VIDEO_INPUT="$VIDEO_INPUT_V4L2"
 
 INPUT="
 	-thread_queue_size 1024 $AUDIO_INPUT
@@ -135,9 +135,18 @@ STREAM_2=stream2
 STREAM_3=stream3
 STREAM_4=stream4
 
+echo "Protocol (rtmp/rtsp): $PROTOCOL"
+AUDIO_CODEC=unknown
+if [ "$PROTOCOL" = "rtmp" ]; then
+  AUDIO_CODEC=aac
+fi
+if [ "$PROTOCOL" = "rtsp" ]; then
+  AUDIO_CODEC=libopus
+fi
+
 # "-c:a aac" for mpeg4 audio, "-c:a libopus" for Opus (which is compatible with WebRTC)
 ENCODE_AUDIO="
-	-c:a libopus
+	-c:a $AUDIO_CODEC
 	-map 0:a:0 -ac:a:0 1 -b:a:0 128k
 	-map 0:a:0 -ac:a:1 1 -b:a:1 64k
 	-map 0:a:0 -ac:a:2 1 -b:a:2 48k
@@ -162,13 +171,18 @@ mkdir -p recordings
 FILENAME=recordings/$(date +%Y-%m-%d_%H:%M:%S).mkv
 OUTPUT="$OUTPUT|[select=\'a:0,v:4\']$FILENAME"
 
-# For reference, this is what it looks like for RTMP ingest:
-#OUTPUT="$OUTPUT|[f=flv:select=\'a:0,v:0\']rtmp://$SERVER1/$APP/$STREAM_1"
-# But we're now using RTSP because it can carry more codecs:
-OUTPUT="$OUTPUT|[f=rtsp:rtsp_transport=tcp:select=\'a:0,v:0\']rtsp://$SERVER1:8554/$APP/$STREAM_1"
-OUTPUT="$OUTPUT|[f=rtsp:rtsp_transport=tcp:select=\'a:0,v:1\']rtsp://$SERVER1:8554/$APP/$STREAM_2"
-OUTPUT="$OUTPUT|[f=rtsp:rtsp_transport=tcp:select=\'a:1,v:2\']rtsp://$SERVER1:8554/$APP/$STREAM_3"
-OUTPUT="$OUTPUT|[f=rtsp:rtsp_transport=tcp:select=\'a:2,v:3\']rtsp://$SERVER1:8554/$APP/$STREAM_4"
+if [ "$PROTOCOL" = "rtmp" ]; then
+  OUTPUT="$OUTPUT|[f=flv:select=\'a:0,v:0\']rtmp://$SERVER1/$APP/$STREAM_1"
+  OUTPUT="$OUTPUT|[f=flv:select=\'a:0,v:1\']rtmp://$SERVER1/$APP/$STREAM_2"
+  OUTPUT="$OUTPUT|[f=flv:select=\'a:1,v:2\']rtmp://$SERVER1/$APP/$STREAM_3"
+  OUTPUT="$OUTPUT|[f=flv:select=\'a:2,v:3\']rtmp://$SERVER1/$APP/$STREAM_4"
+fi
+if [ "$PROTOCOL" = "rtsp" ]; then
+  OUTPUT="$OUTPUT|[f=rtsp:rtsp_transport=tcp:select=\'a:0,v:0\']rtsp://$SERVER1:8554/$APP/$STREAM_1"
+  OUTPUT="$OUTPUT|[f=rtsp:rtsp_transport=tcp:select=\'a:0,v:1\']rtsp://$SERVER1:8554/$APP/$STREAM_2"
+  OUTPUT="$OUTPUT|[f=rtsp:rtsp_transport=tcp:select=\'a:1,v:2\']rtsp://$SERVER1:8554/$APP/$STREAM_3"
+  OUTPUT="$OUTPUT|[f=rtsp:rtsp_transport=tcp:select=\'a:2,v:3\']rtsp://$SERVER1:8554/$APP/$STREAM_4"
+fi
 if [ "${YOUTUBE_STREAM_KEY-}" ]; then
   # Note: this won't work unless we switch back to AAC / MP4 audio
   # (because RTMP doesn't support Opus audio)
